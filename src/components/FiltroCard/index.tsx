1befd7c2cce1bbc03e78bc7cba7e32f381d97435
Manoela Coelho Silva <manoelacs62@gmail.com>
2023-11-08 20:28:27 -0300

    fazendo e mostrando a filtragem das tarefas

--- src/components/FiltroCard/index.tsx
+++ src/components/FiltroCard/index.tsx
@@ -1,16 +1,54 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { alterarFiltro } from '../../store/reducers/filtro'
 import * as Style from './styles'
+import * as enums from '../../utils/enums/Tarefa'
+import { RootReducer } from '../../store'
 
 export type Props = {
-  ativo?: boolean
-  contador: number
   legenda: string
+  criterio: 'prioridade' | 'status' | 'todas'
+  valor?: enums.Prioridade | enums.Status
 }
 
-const FiltroCard = ({ ativo, contador, legenda }: Props) => (
-  <Style.Card ativo={ativo}>
-    <Style.Contador>{contador}</Style.Contador>
-    <Style.Label>{legenda}</Style.Label>
-  </Style.Card>
-)
+const FiltroCard = ({ legenda, criterio, valor }: Props) => {
+  const dispatch = useDispatch()
+  const { filtro, tarefas } = useSelector((state: RootReducer) => state)
+
+  const verificaEstaAtivo = () => {
+    const mesmoCriterio = filtro.criterio === criterio
+    const mesmoValor = filtro.valor === valor
+
+    return mesmoCriterio && mesmoValor
+  }
+
+  const contarTarefas = () => {
+    if (criterio === 'todas') return tarefas.itens.length
+    if (criterio === 'prioridade') {
+      return tarefas.itens.filter((item) => item.prioridade === valor).length
+    }
+    if (criterio === 'status') {
+      return tarefas.itens.filter((item) => item.status === valor).length
+    }
+  }
+
+  const filtrar = () => {
+    dispatch(
+      alterarFiltro({
+        criterio,
+        valor
+      })
+    )
+  }
+
+  const ativo = verificaEstaAtivo()
+  const contador = contarTarefas()
+
+  return (
+    <Style.Card ativo={ativo} onClick={filtrar}>
+      <Style.Contador>{contador}</Style.Contador>
+      <Style.Label>{legenda}</Style.Label>
+    </Style.Card>
+  )
+}
 
 export default FiltroCard
